f68d512c87e9422f73ab06737e0ea03eee2a8168
Helder Ribeiro <helder@discor.de>
2015-01-17 10:32:38 -0200

    Remove commented-out code

--- client/application/templates/profile.js
+++ client/application/templates/profile.js
@@ -1,9 +1,3 @@
-// Template.profile.events({
-//   'click .update-profile': function () {
-//     Router.go('home');
-//   }
-// });
-
 AutoForm.addHooks(null, {
   // Called when any operation succeeds, where operation will be
   // "insert", "update", "submit", or the method name.
